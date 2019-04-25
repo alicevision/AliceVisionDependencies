5203a4639598d1e22ecc118facb95ab98f0dbb6c
Grégoire De Lillo <gregoire-dl@users.noreply.github.com>
2019-04-25 16:02:49 +0200

    [openexr] Build OpenEXR and IlmBase together

--- ci/install-openexr.sh
+++ ci/install-openexr.sh
@@ -10,40 +10,21 @@ export OPENEXR_INSTALL="$2"
 
 export OPENEXR_ROOT="${TRAVIS_BUILD_DIR}/openexr-${OPENEXR_VERSION}"
 export OPENEXR_SOURCE="${OPENEXR_ROOT}/source"
-
-export ILMBASE_BUILD="${OPENEXR_ROOT}/build/IlmBase"
-export OPENEXR_BUILD="${OPENEXR_ROOT}/build/OpenEXR"
-
-
+export OPENEXR_BUILD="${OPENEXR_ROOT}/build"
 
 echo "Download OpenEXR"
-mkdir --parent "$ILMBASE_BUILD"
 mkdir --parent "$OPENEXR_BUILD"
 mkdir --parent "$OPENEXR_INSTALL"
 
-
 git clone --depth 1 --branch "v${OPENEXR_VERSION}" https://github.com/openexr/openexr.git "$OPENEXR_SOURCE"
-
-
-echo "Build IlmBase"
-cd $ILMBASE_BUILD
-
-cmake -DCMAKE_BUILD_TYPE=Release \
-	  -DCMAKE_INSTALL_PREFIX=$OPENEXR_INSTALL \
-	  "$OPENEXR_SOURCE/IlmBase"
-	  
-make -j 2 
-
-echo "Install IlmBase"
-make install
 
 echo "Build OpenEXR"
 cd $OPENEXR_BUILD
 
 cmake -DCMAKE_BUILD_TYPE=Release \
-	  -DILMBASE_PACKAGE_PREFIX=$OPENEXR_INSTALL \
+	  -DOPENEXR_BUILD_PYTHON_LIBS=OFF \
 	  -DCMAKE_INSTALL_PREFIX=$OPENEXR_INSTALL \
-	  "$OPENEXR_SOURCE/OpenEXR"
+	  "$OPENEXR_SOURCE"
 	  
 make -j 2 
 
